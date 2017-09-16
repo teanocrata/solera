eff71b40134ef7c740be4bba69cbaeb23e46b938
Isabel Muñoz <teanocrata@gmail.com>
2017-09-16 13:51:05 +0200

    Initial pages

--- src/App.js
+++ src/App.js
@@ -6,15 +6,37 @@ import SNavbar from "./navbar/SNavbar";
 import Page from "./page/Page";
 import logo from "./solera_logo_80x80_green.png";
 
+import meetUs from "./resources/pages/guidedVisits.json";
+import lessons from "./resources/pages/lessons.json";
 import page from "./resources/pages/page.json";
 
 class App extends Component {
   render() {
     const routes = [
       {
-        path: "/",
+        path: "/guidedVisits",
+        main: () => <Page page={meetUs} />,
+        text: "¡Conócenos!"
+      },
+      {
+        path: "/lessons",
+        main: () => <Page page={lessons} />,
+        text: "Clases"
+      },
+      {
+        path: "/ponies",
         main: () => <Page page={page} />,
-        text: "Home"
+        text: "Ponies"
+      },
+      {
+        path: "/birthdays",
+        main: () => <Page page={page} />,
+        text: "Cumpleaños"
+      },
+      {
+        path: "/stabling",
+        main: () => <Page page={page} />,
+        text: "Pupilaje"
       }
     ];
     return (
